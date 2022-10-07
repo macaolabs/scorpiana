6f57a03b696d56742d38ccb7df2c03f269e0c43d
RuGe <70239531+ruge0326@users.noreply.github.com>
2022-10-06 23:16:27 -0600

    init multitouch

--- App.tsx
+++ App.tsx
@@ -1,61 +1,13 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import MultiTouch from './src/touch-events/multi-touch';
+import SingleTouch from './src/touch-events/single-touch';
 
-import React, {useEffect, useState, type PropsWithChildren} from 'react';
-import io from 'socket.io-client';
-import {BasicWorld} from './src/basic-world';
-
-const socket = io('https://realtime-g2g4oq25qa-uc.a.run.app');
-
-const App = () => {
-  const [isConnected, setIsConnected] = useState(socket.connected);
-  const [pong, setPong] = useState<any>({});
-  const [ping, setPing] = useState<any>({});
-
-  useEffect(() => {
-    socket.on('connect', () => {
-      setIsConnected(true);
-    });
-
-    socket.on('disconnect', () => {
-      setIsConnected(false);
-    });
-
-    socket.on('s:status', (message: any) => {
-      setPong({
-        ...message,
-        time: new Date().getTime(),
-      });
-    });
-
-    return () => {
-      socket.off('connect');
-      socket.off('disconnect');
-      socket.off('pong');
-    };
-  }, []);
-
-  const sendPing = () => {
-    const ping = {
-      message: 'ping',
-      time: new Date().getTime(),
-    };
-    setPing(ping);
-    socket.emit('c:move', ping);
-  };
-
-  return (
-    <>
-      <BasicWorld></BasicWorld>
-    </>
-  );
+const Flex = () => {
+  return <MultiTouch />;
+  // return <SingleTouch/>
 };
 
-export default App;
+const styles = StyleSheet.create({});
+
+export default Flex;
